816fe67eeb0049bf23806467af2075b3126f7156
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-10-02 08:21:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fronta
 video = cv2.VideoCapture(0)
 detectionFrameColor = (0, 255, 0)
 detectionFrameThickness = 2
+detectionStrength = 1.2
 
 while True:
   
     ret, frame = video.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=detectionStrength, minNeighbors=5)
 
     if(type(faces) == numpy.ndarray):
         print("face detected")
