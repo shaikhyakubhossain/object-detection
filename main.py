3c9b4e14e9c5d28ceafa931259ab469ef0916eaf
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-10-02 20:53:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fronta
 video = cv2.VideoCapture(0)
 detectionFrameColor = (0, 255, 0)
 detectionFrameThickness = 2
-detectionStrength = 1.2
+detectionStrength = 1.1
+
+detectionTextColor = (0, 255, 0)
+detectionTextThickness = 2
+detectionTextSize = 1
 
 while True:
   
@@ -19,9 +23,10 @@ while True:
     faces = face_cascade.detectMultiScale(gray, scaleFactor=detectionStrength, minNeighbors=5)
 
     if(type(faces) == numpy.ndarray):
-        print("face detected")
+        # print("face detected")
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y), (x + w, y + h), detectionFrameColor, detectionFrameThickness)
+            cv2.putText(frame, "Face Detected", (x, y + -10), cv2.FONT_HERSHEY_SIMPLEX, detectionTextSize, detectionTextColor, detectionTextThickness)
 
     cv2.imshow('Face Detection', frame)
 
